a797ba5590fb2169a0c20bd250c6f05926bbfade
Brett Timperman <user@Bretts-MacBook-Air.local>
2015-07-06 15:48:25 -0400

    load subpages with page, enhanced search

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,39 @@
 var express = require('express'),
     gmailer = require('../lib/gmailer');
 
+var renderDefaultPage = function(req, res, next) {
+  db.findPage({ slug: db.getOption('defaultpage').value }, function(err, page) {
+    if (err) res.render('index');
+    else res.render('index', page);
+  });
+};
+
 module.exports = function(db) {
   var router = express.Router();
 
-  router.get('/', function(req, res, next) {
-    db.findPage({ slug: db.getOption('defaultpage').value }, function(err, page) {
-      if (err) res.render('index');
-      else res.render('index', page);
-    });
-  });
+  router.get('/', renderDefaultPage);
 
   router.get('/search', function(req, res, next) {
     db.getSearchIndex().search({ query: { "*": [req.query.q.split(' ')] }}, function(err, results) {
-      if (err) res.render('error');
-      else res.render('search', results);
+      if (err) {
+        console.log('search error', err);
+        res.render('error');
+      }
+      else {
+        if (req.query.tag) {
+          results.hits = _.filter(results.hits, function(hit) {
+            return ( hit.document.tags === "" || hit.document.tags.indexOf(req.query.tag) >= 0 );
+          });
+        }
+        results.hits = _.map(results.hits, function(hit) {
+          if (hit.document.slug.indexOf("-hash-") > 0) {
+            hit.document = _.extend(hit.document, { slug: hit.document.slug.replace('-hash-', '#') });
+          }
+
+          return hit;
+        });
+        res.render('search', results);
+      }
     });
   });
 
@@ -24,14 +43,19 @@ module.exports = function(db) {
   });
 
   router.get('/:slug', function(req, res, next) {
-    db.findPage({ slug: req.params.slug }, function(err, page) {
-      if (err) {
-        console.log(req.params.slug, 'cms page not found');
-        res.render(req.params.slug);
-      }
-      else
-        res.render(req.params.slug, page);
-    });
+    if ( req.params.slug ) {
+      var pageSlug = req.params.slug.split(".");
+      db.findPage({ slug: pageSlug[0] }, function(err, page) {
+        if (err) {
+          console.log(pageSlug[0], 'cms page not found');
+          res.render(pageSlug[0]);
+        }
+        else
+          res.render(pageSlug[0], page);
+      });
+    }
+    else
+      renderDefaultPage(req, res, next);
   });
 
   return router;
